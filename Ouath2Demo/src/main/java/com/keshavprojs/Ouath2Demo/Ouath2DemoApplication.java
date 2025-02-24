9c5d40aafb1277a1d7ee766e967d2eda9d457cd1
Keshav <rathore.m@Infosys.com>
2025-02-24 11:33:02 +0530

    changes

---
+++ Ouath2Demo/src/main/java/com/keshavprojs/Ouath2Demo/Ouath2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.keshavprojs.Ouath2Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ouath2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ouath2DemoApplication.class, args);
+	}
+
+}
--- Ouath2Demo/src/test/java/com/keshavprojs/Ouath2Demo/Ouath2DemoApplicationTests.java
+++ Ouath2Demo/src/test/java/com/keshavprojs/Ouath2Demo/Ouath2DemoApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package com.keshavprojs.Ouath2Demo;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class Ouath2DemoApplicationTests {
 	}
 
 }
+*/
