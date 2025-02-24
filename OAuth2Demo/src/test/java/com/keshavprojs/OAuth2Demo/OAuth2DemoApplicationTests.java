d65b22d8f190b19a58e727eaa473a1d378bd35ff
Keshav <rathore.m@Infosys.com>
2025-02-24 10:12:01 +0530

    first commit

---
+++ OAuth2Demo/src/test/java/com/keshavprojs/OAuth2Demo/OAuth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keshavprojs.OAuth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
