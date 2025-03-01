a816b376e69a1b5f647874ad431df042fecddbeb
kshav99 <47470399+kshav99@users.noreply.github.com>
2025-03-01 21:08:59 +0530

    added dataflow engine

---
+++ DataFlowEngine/src/main/java/com/keshavprojs/DataFlowEngine/DataFlowEngineApplication.java
@@ -0,0 +1,13 @@
+package com.keshavprojs.DataFlowEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFlowEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataFlowEngineApplication.class, args);
+	}
+
+}
---
+++ DataFlowEngine/src/test/java/com/keshavprojs/DataFlowEngine/DataFlowEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keshavprojs.DataFlowEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFlowEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
